289c1ec721c32ababa3cb7e510ac26ffdf1a306a
Kharitonov Sergey <skharitonov95@gmail.com>
2020-03-28 20:29:35 +0300

    added configs for gradle project

---
+++ buildSrc/src/main/java/Versions.java
@@ -0,0 +1,3 @@
+public final class Versions {
+	public static final String GUAVA = "28.2-jre";
+}
---
+++ hw01-gradle/src/main/java/ru/otus/HelloOtus.java
@@ -0,0 +1,13 @@
+package ru.otus;
+
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+public class HelloOtus {
+	public static void main(String[] args) {
+		final List<Integer> numbers = Lists.newArrayList(1, 2, 3, 4, 5);
+
+		System.out.println(numbers);
+	}
+}
