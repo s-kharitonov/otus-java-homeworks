6fe5218574b75c42953d261d6d89c08a55ebd549
Kharitonov Sergey <skharitonov95@gmail.com>
2020-04-19 20:20:01 +0300

    added custom exception IllegalUsingAnnotationException

---
+++ hw03-test-framework/src/main/java/ru/otus/exceptions/IllegalUsingAnnotationsException.java
@@ -0,0 +1,8 @@
+package ru.otus.exceptions;
+
+public class IllegalUsingAnnotationsException extends Exception {
+
+	public IllegalUsingAnnotationsException(final String message) {
+		super(message);
+	}
+}
