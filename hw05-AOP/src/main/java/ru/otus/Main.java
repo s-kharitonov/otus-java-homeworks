46a63a1b1679778244c5b010fd595135d45f1635
Sergey Kharitonov <skharitonov95@gmail.com>
2020-05-05 19:39:33 +0300

    added constants

--- hw05-AOP/src/main/java/ru/otus/Main.java
+++ hw05-AOP/src/main/java/ru/otus/Main.java
@@ -1,7 +1,16 @@
 package ru.otus;
 
+import ru.otus.calculators.Calculator;
+import ru.otus.calculators.impl.Addition;
+import ru.otus.calculators.impl.Division;
+import ru.otus.proxy.IOC;
+
 public class Main {
 	public static void main(String[] args) {
+		final Calculator addition = IOC.createCalculator(new Addition());
+		final Calculator division = IOC.createCalculator(new Division());
 
+		addition.calculate(1, 1);
+		division.calculate(1, 1);
 	}
 }
