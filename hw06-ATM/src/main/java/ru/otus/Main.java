9112d4f5469d4e3a6cde1f5eff6576a53611cc67
Sergey Kharitonov <skharitonov95@gmail.com>
2020-05-17 19:15:23 +0300

    added throws exception

--- hw06-ATM/src/main/java/ru/otus/Main.java
+++ hw06-ATM/src/main/java/ru/otus/Main.java
@@ -4,12 +4,13 @@ import ru.otus.atms.Atm;
 import ru.otus.dao.AtmDAO;
 import ru.otus.dao.impl.BasicAtmDAO;
 import ru.otus.domain.Banknote;
+import ru.otus.exceptions.AtmModificationException;
 
 public class Main {
 
 	private static final AtmDAO atmDAO = new BasicAtmDAO();
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws AtmModificationException {
 		final Atm atm = atmDAO.initAtm();
 
 		System.out.println("balance: " + atm.calculateBalance());
