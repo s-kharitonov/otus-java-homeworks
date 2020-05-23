d9e5382210024bb06b299b9e616d8536ec85ab8b
Sergey Kharitonov <skharitonov95@gmail.com>
2020-05-23 19:34:18 +0300

    refactoring

---
+++ hw06-ATM/src/main/java/ru/otus/domain/cells/Cell.java
@@ -0,0 +1,15 @@
+package ru.otus.domain.cells;
+
+import ru.otus.domain.Banknote;
+
+import java.util.NoSuchElementException;
+
+public interface Cell {
+	Banknote getBanknote() throws NoSuchElementException;
+
+	void addBanknote(final Banknote banknote);
+
+	int calculateBalance();
+
+	int size();
+}
