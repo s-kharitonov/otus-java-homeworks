43e4c52c70d560d39776ee2b34bd6e20f7fb78ea
Sergey Kharitonov <skharitonov95@gmail.com>
2020-06-04 19:16:17 +0300

    fixed SimpleTypeAdapterTest

--- hw08-json-object-writer/src/test/java/ru/otus/adapters/impl/SimpleTypeAdapterTest.java
+++ hw08-json-object-writer/src/test/java/ru/otus/adapters/impl/SimpleTypeAdapterTest.java
@@ -5,12 +5,13 @@ import com.google.gson.GsonBuilder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class SimpleTypeAdapterTest {
 
@@ -24,119 +25,127 @@ class SimpleTypeAdapterTest {
 	@ParameterizedTest
 	@ValueSource(strings = {"Hello", "World", "!", ""})
 	void getJsonValueString(final String simpleString) {
-		final String gsonValue = gson.toJson(simpleString);
 		final String simpleJsonValue = new SimpleTypeAdapter(simpleString)
 				.getJsonValue()
 				.toString();
+		final String result = gson.fromJson(simpleJsonValue, String.class);
 
-		assertEquals(gsonValue, simpleJsonValue);
+		assertTrue(simpleString.equalsIgnoreCase(result));
 	}
 
 	@ParameterizedTest
 	@ValueSource(booleans = {true, false})
 	void getJsonValueBoolean(final boolean value) {
-		final String gsonValue = gson.toJson(value);
 		final String simpleJsonValue = new SimpleTypeAdapter(value)
 				.getJsonValue()
 				.toString();
+		final boolean result = gson.fromJson(simpleJsonValue, Boolean.class);
 
-		assertEquals(gsonValue, simpleJsonValue);
+		assertEquals(result, value);
 	}
 
 	@ParameterizedTest
 	@ValueSource(chars = {'h', 'e', 'l', 'o'})
 	void getJsonValueCharacters(final char value) {
-		final String gsonValue = gson.toJson(value);
 		final String simpleJsonValue = new SimpleTypeAdapter(value)
 				.getJsonValue()
 				.toString();
+		final char result = gson.fromJson(simpleJsonValue, Character.class);
 
-		assertEquals(gsonValue, simpleJsonValue);
+		assertEquals(result, value);
 	}
 
 	@ParameterizedTest
 	@ValueSource(bytes = {Byte.MAX_VALUE, Byte.MIN_VALUE})
 	void getJsonValueBytes(final byte value) {
-		final String gsonValue = gson.toJson(value);
 		final String simpleJsonValue = new SimpleTypeAdapter(value)
 				.getJsonValue()
 				.toString();
+		final byte result = gson.fromJson(simpleJsonValue, Byte.class);
 
-		assertEquals(gsonValue, simpleJsonValue);
+		assertEquals(result, value);
 	}
 
 	@ParameterizedTest
 	@ValueSource(shorts = {Short.MIN_VALUE, Short.MAX_VALUE})
 	void getJsonValueShorts(final short value) {
-		final String gsonValue = gson.toJson(value);
 		final String simpleJsonValue = new SimpleTypeAdapter(value)
 				.getJsonValue()
 				.toString();
+		final short result = gson.fromJson(simpleJsonValue, Short.class);
 
-		assertEquals(gsonValue, simpleJsonValue);
+		assertEquals(result, value);
 	}
 
 	@ParameterizedTest
 	@ValueSource(ints = {Integer.MAX_VALUE, Integer.MIN_VALUE})
 	void getJsonValueInt(final int value) {
-		final String gsonValue = gson.toJson(value);
 		final String simpleJsonValue = new SimpleTypeAdapter(value)
 				.getJsonValue()
 				.toString();
+		final int result = gson.fromJson(simpleJsonValue, Integer.class);
 
-		assertEquals(gsonValue, simpleJsonValue);
+		assertEquals(result, value);
 	}
 
 	@ParameterizedTest
 	@ValueSource(longs = {Long.MIN_VALUE, Long.MAX_VALUE})
 	void getJsonValueLong(final long value) {
-		final String gsonValue = gson.toJson(value);
 		final String simpleJsonValue = new SimpleTypeAdapter(value)
 				.getJsonValue()
 				.toString();
+		final long result = gson.fromJson(simpleJsonValue, Long.class);
 
-		assertEquals(gsonValue, simpleJsonValue);
+		assertEquals(result, value);
 	}
 
 	@ParameterizedTest
 	@ValueSource(floats = {Float.MIN_VALUE, Float.MAX_VALUE})
 	void getJsonValueFloat(final float value) {
-		final String gsonValue = gson.toJson(value);
 		final String simpleJsonValue = new SimpleTypeAdapter(value)
 				.getJsonValue()
 				.toString();
+		final float result = gson.fromJson(simpleJsonValue, Float.class);
 
-		assertEquals(gsonValue, simpleJsonValue);
+		assertEquals(result, value);
 	}
 
 	@ParameterizedTest
 	@ValueSource(doubles = {Double.MIN_VALUE, Double.MAX_VALUE})
 	void getJsonValueDouble(final double value) {
-		final String gsonValue = gson.toJson(value);
 		final String simpleJsonValue = new SimpleTypeAdapter(value)
 				.getJsonValue()
 				.toString();
+		final double result = gson.fromJson(simpleJsonValue, Double.class);
 
-		assertEquals(gsonValue, simpleJsonValue);
+		assertEquals(result, value);
 	}
 
 	@Test
 	void getJsonValueBigInt() {
-		final String gsonValue = gson.toJson(BigInteger.TEN);
 		final String simpleJsonValue = new SimpleTypeAdapter(BigInteger.TEN)
 				.getJsonValue()
 				.toString();
+		final BigInteger result = gson.fromJson(simpleJsonValue, BigInteger.class);
 
-		assertEquals(gsonValue, simpleJsonValue);
+		assertEquals(result, BigInteger.TEN);
 	}
 
 	@Test
 	void getJsonValueBigDecimal() {
-		final String gsonValue = gson.toJson(BigDecimal.TEN);
 		final String simpleJsonValue = new SimpleTypeAdapter(BigDecimal.TEN)
 				.getJsonValue()
 				.toString();
+		final BigDecimal result = gson.fromJson(simpleJsonValue, BigDecimal.class);
 
-		assertEquals(gsonValue, simpleJsonValue);
+		assertEquals(result, BigDecimal.TEN);
+	}
+
+	@ParameterizedTest
+	@NullSource
+	void getJsonValueWithEmptyParam(final Object value) {
+		assertThrows(NullPointerException.class, () -> {
+			new SimpleTypeAdapter(value);
+		});
 	}
 }
