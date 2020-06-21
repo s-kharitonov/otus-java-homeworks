38fcc418ac1bbe133ee4cd04a125149cc4997432
Sergey Kharitonov <skharitonov95@gmail.com>
2020-06-21 14:47:26 +0300

    save class info

--- hw09-ORM/src/main/java/ru/otus/jdbc/mapper/impl/BasicEntityClassMetaData.java
+++ hw09-ORM/src/main/java/ru/otus/jdbc/mapper/impl/BasicEntityClassMetaData.java
@@ -11,44 +11,68 @@ import java.util.stream.Collectors;
 
 public class BasicEntityClassMetaData<T> implements EntityClassMetaData<T> {
 
-	private final Class<?> clazz;
+	private final String name;
+	private final Constructor<T> constructor;
+	private final Field id;
+	private final List<Field> allFields;
+	private final List<Field> fieldsWithoutId;
 
 	public BasicEntityClassMetaData(final Class<?> clazz) {
-		this.clazz = clazz;
+		this.name = clazz.getSimpleName();
+		this.constructor = extractDefaultConstructor(clazz);
+		this.allFields = extractFields(clazz);
+		this.id = extractFieldId(allFields);
+		this.fieldsWithoutId = extractFieldsWithoutId(allFields);
 	}
 
 	@Override
 	public String getName() {
-		return clazz.getSimpleName();
+		return name;
 	}
 
 	@Override
-	@SuppressWarnings("unchecked")
 	public Constructor<T> getConstructor() {
+		return constructor;
+	}
+
+	@Override
+	public Field getIdField() {
+		return id;
+	}
+
+	@Override
+	public List<Field> getAllFields() {
+		return allFields;
+	}
+
+	@Override
+	public List<Field> getFieldsWithoutId() {
+		return fieldsWithoutId;
+	}
+
+	@SuppressWarnings("unchecked")
+	private Constructor<T> extractDefaultConstructor(final Class<?> clazz) {
 		return (Constructor<T>) Arrays.stream(clazz.getConstructors())
 				.filter((constructor) -> constructor.getParameterCount() == 0)
 				.findFirst()
 				.orElseThrow(() -> new RuntimeException("The default constructor is overridden!"));
 	}
 
-	@Override
-	public Field getIdField() {
+	private List<Field> extractFields(final Class<?> clazz) {
 		return Arrays.stream(clazz.getDeclaredFields())
+				.collect(Collectors.toList());
+	}
+
+	private List<Field> extractFieldsWithoutId(final List<Field> fields) {
+		return fields.stream()
+				.filter(field -> !field.isAnnotationPresent(Id.class))
+				.collect(Collectors.toList());
+	}
+
+	private Field extractFieldId(final List<Field> fields) {
+		return fields.stream()
 				.filter(field -> field.isAnnotationPresent(Id.class))
 				.findFirst()
 				.orElseThrow();
-	}
-
-	@Override
-	public List<Field> getAllFields() {
-		return Arrays.stream(clazz.getDeclaredFields())
-				.collect(Collectors.toList());
-	}
-
-	@Override
-	public List<Field> getFieldsWithoutId() {
-		return Arrays.stream(clazz.getDeclaredFields())
-				.filter(field -> !field.isAnnotationPresent(Id.class))
-				.collect(Collectors.toList());
 	}
 }
