ba17f12b0fb4ddc99dd7b77340a615742616b53e
Sergey Kharitonov <skharitonov95@gmail.com>
2020-06-21 14:25:19 +0300

    save queries

--- hw09-ORM/src/main/java/ru/otus/jdbc/mapper/impl/BasicEntitySQLMetaData.java
+++ hw09-ORM/src/main/java/ru/otus/jdbc/mapper/impl/BasicEntitySQLMetaData.java
@@ -9,41 +9,61 @@ import java.util.stream.Collectors;
 
 public class BasicEntitySQLMetaData<T> implements EntitySQLMetaData {
 
-	private final EntityClassMetaData<T> metaData;
+	private final String selectAll;
+	private final String selectById;
+	private final String insert;
+	private final String update;
 
 	public BasicEntitySQLMetaData(final EntityClassMetaData<T> metaData) {
-		this.metaData = metaData;
+		this.selectAll = createSelectAllQuery(metaData);
+		this.selectById = createSelectByIdQuery(metaData);
+		this.insert = createInsertQuery(metaData);
+		this.update = createUpdateQuery(metaData);
 	}
 
 	@Override
 	public String getSelectAllSql() {
-		return String.format("select * from %s", metaData.getName());
+		return selectAll;
 	}
 
 	@Override
 	public String getSelectByIdSql() {
-		return String.format("select * from %s where %s = ?", metaData.getName(), getIdFieldName());
+		return selectById;
 	}
 
 	@Override
 	public String getInsertSql() {
+		return insert;
+	}
+
+	@Override
+	public String getUpdateSql() {
+		return update;
+	}
+
+	private String createUpdateQuery(final EntityClassMetaData<T> metaData) {
+		return String.format("update %s set (%s) = (%s) where %s = ?",
+				metaData.getName(),
+				extractColumnsNames(metaData.getFieldsWithoutId()),
+				getParameters(metaData.getFieldsWithoutId()),
+				metaData.getIdField().getName());
+	}
+
+	private String createInsertQuery(final EntityClassMetaData<T> metaData) {
 		return String.format("insert into %s (%s) values (%s)",
 				metaData.getName(),
 				extractColumnsNames(metaData.getAllFields()),
 				getParameters(metaData.getAllFields()));
 	}
 
-	@Override
-	public String getUpdateSql() {
-		return String.format("update %s set (%s) = (%s) where %s = ?",
-				metaData.getName(),
-				extractColumnsNames(metaData.getFieldsWithoutId()),
-				getParameters(metaData.getFieldsWithoutId()),
-				getIdFieldName());
+	private String createSelectAllQuery(final EntityClassMetaData<T> metaData) {
+		return String.format("select * from %s", metaData.getName());
 	}
 
-	private String getIdFieldName() {
-		return metaData.getIdField().getName();
+	private String createSelectByIdQuery(final EntityClassMetaData<T> metaData) {
+		return String.format("select * from %s where %s = ?",
+				metaData.getName(),
+				metaData.getIdField().getName());
 	}
 
 	private String getParameters(final List<Field> fields) {
