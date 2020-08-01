8f241c9473fba1e26688b31272e0438dfc006004
Sergey Kharitonov <skharitonov95@gmail.com>
2020-08-01 14:57:20 +0300

    added cache

---
+++ hw14-Spring-MVC/src/main/java/ru/otus/cache/impl/BasicCacheKeeper.java
@@ -0,0 +1,64 @@
+package ru.otus.cache.impl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.otus.cache.CacheKeeper;
+import ru.otus.listeners.Listener;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.WeakHashMap;
+
+public class BasicCacheKeeper<K, V> implements CacheKeeper<K, V> {
+
+	private static final Logger logger = LoggerFactory.getLogger(BasicCacheKeeper.class);
+
+	private final List<Listener<K, V>> listeners = new ArrayList<>();
+	private final Map<String, V> cache = new WeakHashMap<>();
+
+	@Override
+	public void put(final K key, final V value) {
+		cache.put(String.valueOf(key), value);
+
+		notifyListeners(key, value, "saved in storage");
+	}
+
+	@Override
+	public void remove(final K key) {
+		final V value = cache.remove(String.valueOf(key));
+
+		notifyListeners(key, value, "removed from storage");
+	}
+
+	@Override
+	public V get(final K key) {
+		final V value = cache.get(String.valueOf(key));
+
+		notifyListeners(key, value, "retrieved from storage");
+
+		return value;
+	}
+
+	@Override
+	public void addListener(final Listener<K, V> listener) {
+		listeners.add(listener);
+	}
+
+	@Override
+	public void removeListener(final Listener<K, V> listener) {
+		listeners.remove(listener);
+	}
+
+	private void notifyListeners(final K key, final V value, final String action) {
+		listeners.forEach((listener) -> notifyListener(listener, key, value, action));
+	}
+
+	private void notifyListener(final Listener<K, V> listener, final K key, final V value, final String action) {
+		try {
+			listener.notify(key, value, action);
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+		}
+	}
+}
---
+++ hw14-Spring-MVC/src/main/java/ru/otus/listeners/Listener.java
@@ -0,0 +1,5 @@
+package ru.otus.listeners;
+
+public interface Listener<K, V> {
+	void notify(K key, V value, String action);
+}
